639290f454e928f18844d9da2bff68da4f6ff0f0
Phodal Huang <h@phodal.com>
2021-05-02 10:35:39 +0800

    build: try to set vavlue

--- src/lib.rs
+++ src/lib.rs
@@ -20,16 +20,30 @@ mod tests {
         println!("{:?}", env!("OUT_DIR"));
         assert_eq!(2 + 2, 4);
 
-        let mut params: Vec<&str> = vec![
-            "-p",
-            "src/ffi.rs"
+        let mut params: Vec<String> = vec![
+            "-p".to_string(),
+            "src/ffi.rs".to_string()
         ];
-        let mut parameters: Vec<*mut i8> =
-            params
-                .iter()
-                .map(|s| {
-                    "src/ffi.rs".to_string().as_mut_ptr() as *mut i8
-                }).collect();
+
+        // let args = params.map(|arg| CString::new(arg).unwrap() ).collect::<Vec<CString>>();
+        // // convert the strings to raw pointers
+        // let c_args = args.iter().map(|arg| arg.as_ptr()).collect::<Vec<*const c_char>>();
+
+        // let result = CString::new("hello").unwrap().as_ptr();
+        //
+        // let mut parameters: Vec<*mut i8> =
+        //     params
+        //         .iter()
+        //         .map(|s| {
+        //             // "src/ffi.rs".to_string().as_mut_ptr() as *mut i8
+        //             CString::new(s.to_string()).unwrap().as_ptr() as *mut i8
+        //         }).collect();
+
+        // let mut p = Box::into_raw(Box::new("-p src/ffi.rs")) as *mut i8;
+
+        let mut parameters: Vec<*mut i8> = params.iter().map(|s| {
+            Box::into_raw(Box::new(s.to_string())) as *mut i8
+        }).collect();
 
         unsafe {
             let i = ctags_cli_main(1, parameters.as_mut_ptr());
